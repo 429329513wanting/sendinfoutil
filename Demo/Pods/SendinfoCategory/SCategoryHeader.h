c94d29f79c20e0c0e592119a8fe03c6adeb9bc5f
ghwang <429329513@qq.com>
2018-04-20 16:32:31 +0800

    整理头文件

---
+++ Demo/Pods/SendinfoCategory/SCategoryHeader.h
@@ -0,0 +1,25 @@
+//
+//  SCategoryHeader.h
+//  SendinfoCategory
+//
+//  Created by ghwang on 2018/4/20.
+//
+
+#ifndef SCategoryHeader_h
+#define SCategoryHeader_h
+
+#import "NSDate+Addition.h"
+#import "NSDate+Utilities.h"
+#import "NSMutableDictionary+RXRMultipleItems.h"
+#import "NSString+MD5.h"
+#import "NSString+NSString+RXRURLEscape.h"
+#import "NSString+val.h"
+#import "NSURL+Rexxar.h"
+#import "SendIFAPPDefault.h"
+#import "UIColor+PGHex.h"
+#import "UIColor+Utilty.h"
+#import "UIFont+Common.h"
+#import "UIImage+Handle.h"
+#import "UIView+Toast.h"
+
+#endif /* SCategoryHeader_h */
