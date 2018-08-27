d03a04e2e2b40291aed4a70ec4a42ad693093a27
ghwang <>
2018-08-27 17:38:58 +0800

    ss

--- SendinfoCategory/NSString+val.m
+++ SendinfoCategory/NSString+val.m
@@ -18,8 +18,8 @@
     //    移动号段:134/135/136/137/138/139/150/151/152/157/158/159/182/183/184/187/188/147/178
     //    虚拟运营商:170
     
-    NSString *mobile = @"^1(3[0-9]|4[57]|5[0-35-9]|8[0-9]|7[06-8])\\d{8}$";
-    
+    NSString *mobile = @"^1(2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9])\\d{8}$";
+
     NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", mobile];
     
     return [regextestmobile evaluateWithObject:phoneNum];
